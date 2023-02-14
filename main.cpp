8bce7b4209f736399cb30cd9ca1c317f26315ac4
Amanda Chambless <amchambless@csuchico.edu>
2023-02-13 22:07:58 -0800

    Small bit of code for practice purposes.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// main.cpp for Activity 5 - CSCI430 (software engineering)
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Continuous integration is a wonderful tool." << endl;
+
+    return 0;
+}
